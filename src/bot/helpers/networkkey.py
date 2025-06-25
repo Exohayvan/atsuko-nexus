548a67ab8f9d7de1ac9ffd4c9a18fed68339bf89
ExoHayvan <18trevor3695@gmail.com>
2025-06-25 02:28:39 -0400

    [ADD] Network Key generation based on version

--- src/bot/helpers/networkkey.py
+++ src/bot/helpers/networkkey.py
@@ -3,5 +3,4 @@ import hashlib
 def get_network_key(version):
     combine = f"atsuko-nexus-p2p-{version}"
     NETWORK_KEY = hashlib.sha256(combine.encode()).hexdigest()
-    print(f"{NETWORK_KEY}")
     return NETWORK_KEY
