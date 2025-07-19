2d13e51a1efe668db097e19efad4902179302d0f
ExoHayvan <18trevor3695@gmail.com>
2025-07-18 20:16:00 -0400

    [Removed] PeerEntery and PeerFile struct from bootstrap script

--- src/p2p/bootstrap.go
+++ src/p2p/bootstrap.go
@@ -18,18 +18,6 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-type PeerEntry struct {
-	NodeID   string `yaml:"node_id" json:"node_id"`
-	IPv4     string `yaml:"ipv4" json:"ipv4"`
-	IPv6     string `yaml:"ipv6" json:"ipv6"`
-	Port     int    `yaml:"port" json:"port"`
-	LastSeen string `yaml:"last_seen" json:"last_seen"`
-}
-
-type PeerFile struct {
-	Peers []PeerEntry `yaml:"peers"`
-}
-
 // Bootstrap initializes peer list, adds self, and optionally connects to a bootstrap node
 func Bootstrap() {
 
