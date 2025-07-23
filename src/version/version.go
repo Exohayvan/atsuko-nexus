4a3100387e98202d83cec348e639750c59b74bfc
ExoHayvan <18trevor3695@gmail.com>
2025-07-23 13:56:22 -0400

    bump

--- src/version/version.go
+++ src/version/version.go
@@ -4,8 +4,8 @@ package version
 
 // Current defines the current release version of the application.
 // This value should be updated manually before each release.
-var Current = "v1.4.4-alpha"
-var Future = "v1.4.4-alpha"
+var Current = "v1.4.5-alpha"
+var Future = "v1.4.5-alpha"
 
 // Get returns the current version string.
 // Use this function to programmatically access the application version.
